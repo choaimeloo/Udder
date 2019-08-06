dbad4138c35a444d98d7c6987eb0c0d2c1d733f7
choloo <choaimeloo@gmail.com>
2019-08-06 17:29:35 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jan Cho on 06/08/2019.
+//  Copyright © 2019 Jan Cho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
